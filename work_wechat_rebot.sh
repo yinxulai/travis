e04c14fe6e61d4ec21e86a83f246abe0fa86a103
yinxulai <yinxulai@hotmail.com>
2020-04-11 18:14:45 +0800

    zz

--- work_wechat_rebot.sh
+++ work_wechat_rebot.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
+# 遇到异常主动推出
+-ex
+
+
+# 时间类型
+EVENT_TYPE=$TRAVIS_EVENT_TYPE
+# commit 信息
+COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE
+# 机器人 key
+WORK_WECHAT_ROBOT_KEY=$WORK_WECHAT_ROBOT_KEY
+
+
 # 检查 key
 checkKey() {
   if [ -z $WORK_WECHAT_ROBOT_KEY ]; then
     echo 'WORK_WECHAT_ROBOT_KEY is nil'
     exit 1
   fi
+}
+
+
+# 是否推送
+isPush() {
+  MATCH_STRING='[x] 通知前端工作群'
+  if [[ $COMMIT_MESSAGE == *$MATCH_STRING* ]]; then 
+    return true
+  fi
+
+  return false
+}
+
+# 是否艾特所有人
+isAtAll() {
+  MATCH_STRING='[x] @前端工作群所有人'
+  if [[ $COMMIT_MESSAGE == *$MATCH_STRING* ]]; then 
+    return true
+  fi
+
+  return false
 }
 
 # 推送消息
@@ -27,27 +60,44 @@ pushNotice() {
 
 # 生成消息
 generateMessage() {
-  MESSAGE_FILE=`mktemp`
-  # 头
-  echo "{"                            >> $MESSAGE_FILE
-  echo '"msgtype":"markdown",'        >> $MESSAGE_FILE
-  echo '  "markdown":{'               >> $MESSAGE_FILE
-  echo '    "content":'               >> $MESSAGE_FILE
-  echo '"'                              >> $MESSAGE_FILE
+   MESSAGE_FILE=`mktemp`
+  MESSAGE_FILE='senddata'
+  MESSAGE_TITLE='公共依赖库变更：'
 
-  echo '##### 公共依赖库变更：'           >> $MESSAGE_FILE
-  echo "> $TRAVIS_COMMIT_MESSAGE"       >> $MESSAGE_FILE
+  # 清空
+  echo '' > $MESSAGE_FILE
 
-  echo '"'                              >> $MESSAGE_FILE
-  echo " }"                           >> $MESSAGE_FILE
-  echo "}"                            >> $MESSAGE_FILE
+  # 写入头
+  echo "{"                                          >> $MESSAGE_FILE
+  echo '"msgtype":"markdown",'                      >> $MESSAGE_FILE
+  echo '  "markdown":{'                             >> $MESSAGE_FILE
+  echo '    "content":'                             >> $MESSAGE_FILE
 
-  # TODO: 仅仅只允许 4096 个字节 utf8
+  # 标题 & 正文
+  echo  '"#### '$MESSAGE_TITLE'\\n'                  >> $MESSAGE_FILE
+  # 仅仅只允许 4096 个字节 utf8, 这里简单处理一下
+  # 就当全是汉字（2个字节），最多 2000 个字，4000 字节
   # https://work.weixin.qq.com/help?person_id=1&doc_id=13376
+  echo  '> '${TRAVIS_COMMIT_MESSAGE:0:2000}...'\\n"' >> $MESSAGE_FILE
+
+    # 艾特所有人
+  if [ isAtAll ]; then
+      echo ','                                     >> $MESSAGE_FILE
+      echo '"mentioned_list":["@all"]'             >> $MESSAGE_FILE
+  fi 
+
+  # 写入尾
+  echo " }"                                        >> $MESSAGE_FILE
+  echo "}"                                         >> $MESSAGE_FILE
+
 
   # 使用 echo 向外输出结果
   echo @$MESSAGE_FILE
 }
 
-checkKey
-pushNotice `generateMessage`
+if [ isPush ]; then 
+  checkKey
+  if [ isPush ]; then
+      pushNotice `generateMessage`
+  fi
+fi
